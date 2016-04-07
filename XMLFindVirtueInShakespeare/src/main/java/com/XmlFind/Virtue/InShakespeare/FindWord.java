989cf9ca73225e8d800be9f04bd459c4ec238242
Ryan <ryekayo@yahoo.com>
2016-04-07 13:32:37 -0400

    XMLParserForWordVirtue

---
+++ XMLFindVirtueInShakespeare/src/main/java/com/XmlFind/Virtue/InShakespeare/FindWord.java
@@ -0,0 +1,97 @@
+package com.XmlFind.Virtue.InShakespeare;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+
+import org.apache.log4j.Logger;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+public class FindWord {
+
+	private static final Logger log = Logger.getLogger(FindWord.class);
+	private File xml;
+	public String name;
+	private boolean isFound = false;
+	private int counter = 0;
+
+	public FindWord(File xml) {
+		this.xml = xml;
+		readXML(xml);
+	}
+
+	public void readXML(File xml) {
+		try {
+			SAXBuilder builder = new SAXBuilder();
+			Document document = builder.build(xml);
+			Element rootNode = document.getRootElement();
+			List<Element> play = rootNode.getChildren("ACT");
+			log.debug("Root Element : " + rootNode.getName());
+			log.debug("TITLE OF PLAY : " + rootNode.getChildText("TITLE"));
+			log.debug("----------------------------------------------");
+			for (int i = 0; i < play.size(); i++) {
+				Element shakespeare = play.get(i);
+				List<Element> scenary = shakespeare.getChildren("SCENE");
+				for (int j = 0; j < scenary.size(); j++) {
+					Element scene = scenary.get(j);
+					scene.getChildren("SPEECH");
+					for (Element s : scene.getChildren("SPEECH")) {
+						for (Element part : s.getChildren()) {
+							findVirtue(part);
+
+						}
+					}
+				}
+			}
+			if (isFound) {
+				createDir();
+				XMLOutputter newDoc = new XMLOutputter();
+				newDoc.setFormat(Format.getPrettyFormat());
+				// newDoc.output(play, new FileWriter("Change Directory to the
+				// pointed directory in createDir()" + xml.getName()));
+				log.debug("COUNT FOR " + xml.getName() + ": " + counter);
+				counter = 0;
+			}
+		} catch (IOException io) {
+			log.error(io);
+		} catch (JDOMException jdomex) {
+			log.error(jdomex);
+		} catch (TransformerFactoryConfigurationError e) {
+			log.error(e);
+		} catch (TransformerException a) {
+			log.error(a);
+		}
+	}
+
+	public void findVirtue(Element a)
+			throws JDOMException, IOException, TransformerFactoryConfigurationError, TransformerException {
+		SAXBuilder builder = new SAXBuilder();
+		builder.build(xml);
+		Pattern p = Pattern.compile("(?i)\\bvirtue('?s)?\\b", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(a.getText());
+		List<String> virtue = new ArrayList<String>();
+		while (m.find()) {
+			isFound = true;
+			virtue.add(m.group());
+			log.debug("FOUND : " + m.group());
+			log.debug("REPLACEMENT : " + m.replaceAll("RyanKahil"));
+			a.setText(m.replaceAll("RyanKahil"));
+			counter++;
+		}
+	}
+
+	public void createDir() {
+		// File dir = new File("Change Directory and point to new Folder");
+		// dir.mkdirs();
+	}
+}
---
+++ XMLFindVirtueInShakespeare/src/main/java/com/XmlFind/Virtue/InShakespeare/Tragic.java
@@ -0,0 +1,29 @@
+package com.XmlFind.Virtue.InShakespeare;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class Tragic {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		final Logger log = Logger.getLogger(Tragic.class);
+
+		try {
+			System.out.println("Enter Directory XML files are located in: ");
+			String fileLocation = input.next();
+			File f = new File(fileLocation);
+			ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+			for (int i = 0; i < files.size(); i++) {
+				FindWord word = new FindWord(files.get(i));
+			}
+		} catch (NullPointerException e) {
+			log.error(e);
+		}
+	}
+}
